ecdf5e028fd53f4f63fef80d2877dd702952ea6e
paloma <palomita.c1@gmail.com>
2017-09-04 23:30:54 +0200

    first commit with footer and nav

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './Footer.jsx';
+import Nav from './Nav.jsx';
+
+class App extends React.Component {
+ 
+  render() {
+    return (
+      <div>
+        <Nav/>
+
+
+        <Footer/>
+      </div>
+      );
+  }
+}
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return (
+      <div>
+        <footer id="footer-app" className="py-3 bg-light" >
+          <div className="container text-center">
+            <a href="#copyright">Copyright</a>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer;
