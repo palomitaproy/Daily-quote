9c803b864e23e4e602641f2b7d0a400c1067c12b
paloma <palomita.c1@gmail.com>
2017-09-14 14:43:34 +0200

    update the Titel in index.html

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -16,17 +16,17 @@ class SocialNetworks extends React.Component {
     return (
       <div className="mx-auto social row">
         <div className="col-sm">
-          <FacebookShareButton url="https://yourfortunecookie.herokuapp.com" children="FacebookIcon">
+          <FacebookShareButton url="https://yourfortunecookie.herokuapp.com/" children="FacebookIcon">
             <FacebookIcon  className="mx-auto" size={32} round={true} />
           </FacebookShareButton>
         </div>
         <div className="col-sm">
-          <TwitterShareButton url="https://yourfortunecookie.herokuapp.com" children="TwitterIcon"> 
+          <TwitterShareButton url="https://yourfortunecookie.herokuapp.com/" children="TwitterIcon"> 
             <TwitterIcon className="mx-auto" size={32} round={true} />
           </TwitterShareButton>
         </div> 
         <div className="col-sm">
-          <WhatsappShareButton url="https://yourfortunecookie.herokuapp.com" children="WhatsappIcon">
+          <WhatsappShareButton url="https://yourfortunecookie.herokuapp.com/" children="WhatsappIcon">
             <WhatsappIcon className="mx-auto" size={32} round={true} />
           </WhatsappShareButton>
         </div>
